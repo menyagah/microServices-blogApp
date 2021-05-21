d4e4410b943ed0d515d84b5e0b7902101856af39
menyagah <menyagah27@gmail.com>
2021-05-21 22:22:17 +0300

    Implement get posts route handler Implement the ability to create new post Add code to randomly generate an ID

--- posts/index.js
+++ posts/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');//Add code to randomly generate an ID
 const app = express();
+app.use(bodyParser.json());
 
 const posts = {};
 
 app.get('/posts', (req, res) =>{
-
+    res.send(posts); //Implement get posts route handler
 });
 
 app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex')//implement the ability to create a new post
+    const { title } = req.body;
 
+    posts[id] = {
+        id, title
+    };
+    res.status(201).send(posts[id])
 });
 
 app.listen(4000, () => {
